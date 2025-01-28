2391ebfabe0b7132f4afc099fa4e2a896f604b72
ubiratanfilho <ubfilho16@gmail.com>
2025-01-28 12:09:07 -0300

    Adicionando memória

--- src/agents/graph.py
+++ src/agents/graph.py
@@ -2,19 +2,23 @@ from langgraph.graph import START, END, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition, ToolNode
 from agents.assistant.agent import assistant
 from agents.researcher.agent import researcher
+from langgraph.checkpoint.memory import MemorySaver
 
 # Graph
 builder = StateGraph(MessagesState)
 
-# Define nodes: these do the work
+# Nodes
 builder.add_node("assistant", assistant)
 builder.add_node("researcher", researcher)
 
-# Define edges: these determine how the control flow moves
+# Edges
 builder.add_edge(START, "assistant")
 builder.add_edge("assistant", END)
-react_graph = builder.compile()
 
-# show the graph
+# Compiling
+memory = MemorySaver()
+react_graph = builder.compile(checkpointer=memory)
+
+# Saving the graph
 with open("images/graph.png", "wb") as f:
     f.write(react_graph.get_graph(xray=True).draw_mermaid_png())
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from agents.graph import react_graph
 from langchain_core.messages import HumanMessage
 
+config = {"configurable": {"thread_id": "1"}}
+
 while True:
     question = input("User: ")
-    response = react_graph.invoke({"messages": HumanMessage(content=question)})
+    response = react_graph.invoke({"messages": HumanMessage(content=question)}, config)
     response = response['messages'][-1].content
     print("Bot: ", response)
