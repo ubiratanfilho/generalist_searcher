a3cfe36140feb69bd3609ebf0f1705bf2d4f3b96
ubiratanfilho <ubfilho16@gmail.com>
2025-02-01 18:07:21 -0300

    Flask API

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from utils.graph import graph
-from langchain_core.messages import HumanMessage
+from flask import Flask, request, jsonify
 
-config = {"configurable": {"thread_id": "1"}}
+app = Flask(__name__)
 
-while True:
-    question = input("User: ")
+@app.route('/generate_answer', methods=['POST'])
+def generate_answer():
+    data = request.get_json()
+    question = data.get('question')
+    thread_id = data.get('thread_id')
+    
+    if not question:
+        return jsonify({"error": "Parâmetro 'question' não fornecido."}), 400
+
+    config = {"configurable": {"thread_id": thread_id}}
     response = graph.invoke({"messages": [("user", question)], "question": question, "context": [], "web_search": False}, config)
-    response = response['messages'][-1].content
-    print("Bot: ", response)
+
+    return jsonify({"message": response['messages'][-1].content})
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
