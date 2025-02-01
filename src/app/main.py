813749dfc5ca0b8eafa0de9a1be5411ab6cfc76d
ubiratanfilho <ubfilho16@gmail.com>
2025-02-01 18:17:29 -0300

    Dockerfil e compose

--- src/app/main.py
+++ src/app/main.py
@@ -18,4 +18,4 @@ def generate_answer():
     return jsonify({"message": response['messages'][-1].content})
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5002)
